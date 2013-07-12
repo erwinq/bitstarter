fb5d4972176f8b7be17bd7c671574be516cc772a
Erwin Quimio <equimio@hotmail.com>
2013-07-12 05:21:22 +0000

    Updated web.js read index.html

--- web.js
+++ web.js
@@ -2,11 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var s = new Buffer(fs.readFileSync('index.html'));
+    response.send(s.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
